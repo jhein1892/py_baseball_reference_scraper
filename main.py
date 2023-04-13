5178b4172c3c789b6da6e9f3f17b437dad428757
Jacob Hein <jacobhein1892@gmail.com>
2023-04-13 15:52:02 -0700

    redirecting the project again

--- main.py
+++ main.py
@@ -3,28 +3,46 @@
 # 1) Web Scraping a website
 
 # 2) Multi-Threading to help with performance.
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
+# from selenium import webdriver
+# from selenium.webdriver.common.by import By
+# from selenium.webdriver.common.keys import Keys
 
 
 
-options = webdriver.ChromeOptions()
-options.add_argument('--headless')
+# options = webdriver.ChromeOptions()
+# options.add_argument('--headless')
 
 
-driver = webdriver.Chrome(options=options)
-driver.get('https://www.google.ca')
-# Set up user interaction to determine search
-search_box = driver.find_element(By.NAME, 'q')
-search_box.send_keys("Boston Red Sox ")
-search_box.send_keys(Keys.RETURN)
-driver.implicitly_wait(10)
+# driver = webdriver.Chrome(options=options)
+# driver.get('https://www.google.ca')
+# # Set up user interaction to determine search
+# search_box = driver.find_element(By.NAME, 'q')
+# search_box.send_keys("Boston Red Sox ")
+# search_box.send_keys(Keys.RETURN)
+# driver.implicitly_wait(10)
 
-links = driver.find_elements(By.XPATH, '//div[@id="search"]//a')
+# links = driver.find_elements(By.XPATH, '//div[@id="search"]//a')
 
-for link in links:
-    print(link.get_attribute('href'), "\n")
-# I think at this point we should look into threading. I would like to have some sort of text component as well. Maybe look and see if we can look for top results or something
+# for link in links:
+#     print(link.get_attribute('href'), "\n")
+# # I think at this point we should look into threading. I would like to have some sort of text component as well. Maybe look and see if we can look for top results or something
 
-driver.quit()
+# driver.quit()
+
+## So I'm going to pivot again, and go with scraping for baseball Data
+# I want to print previous stats (not sure if I want to go with season or lifetime averages)'
+# I want to print projected stats
+# I want to current stats up until now -> Extrapolate on how close projections are to current performance.
+
+import requests
+from bs4 import BeautifulSoup
+
+r = requests.get('https://www.baseball-reference.com/players/')
+data = r.text
+
+soup = BeautifulSoup(data, 'html.parser')
+
+print(soup.prettify())
+
+
+
