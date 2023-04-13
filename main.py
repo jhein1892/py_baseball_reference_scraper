2e9e005db80de608ff0e57332b8a095383edf4fa
Jacob Hein <jacobhein1892@gmail.com>
2023-04-12 19:33:32 -0700

    able to see a list of links for search after inital search

--- main.py
+++ main.py
@@ -17,8 +17,19 @@ search_box = driver.find_element(By.NAME, 'q')
 search_box.send_keys("Boston Red Sox")
 search_box.send_keys(Keys.RETURN)
 driver.implicitly_wait(10)
-title = driver.title
+# title = driver.title
+
+
+links = driver.find_elements(By.XPATH, '//div[@id="search"]//a')
+
+# print(links)
+
+for link in links:
+    print(link.get_attribute('href'), "\n")
+#id: res
+#class: yuRUbf
+
 # content = driver.find_element(By.TAG_NAME, "button")
-print(title)
+# print(title)
 
 driver.quit()
