e8364a59c1ff5c9b393944b92ce2bf9c00c9d089
Jacob Hein <jacobhein1892@gmail.com>
2023-04-12 19:52:08 -0700

    added roadmap comments for next steps

--- main.py
+++ main.py
@@ -6,30 +6,25 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+
+
+
 options = webdriver.ChromeOptions()
 options.add_argument('--headless')
 
 
 driver = webdriver.Chrome(options=options)
 driver.get('https://www.google.ca')
-
+# Set up user interaction to determine search
 search_box = driver.find_element(By.NAME, 'q')
-search_box.send_keys("Boston Red Sox")
+search_box.send_keys("Boston Red Sox ")
 search_box.send_keys(Keys.RETURN)
 driver.implicitly_wait(10)
-# title = driver.title
-
 
 links = driver.find_elements(By.XPATH, '//div[@id="search"]//a')
 
-# print(links)
-
 for link in links:
     print(link.get_attribute('href'), "\n")
-#id: res
-#class: yuRUbf
-
-# content = driver.find_element(By.TAG_NAME, "button")
-# print(title)
+# I think at this point we should look into threading. I would like to have some sort of text component as well. Maybe look and see if we can look for top results or something
 
 driver.quit()
